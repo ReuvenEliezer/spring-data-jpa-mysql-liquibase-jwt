55136278bbb179cb481dd6b4a439bad68769899e
Eliezer <reuvene@mprest.com>
2020-12-28 21:00:24 +0200

    update maven version

--- src/test/java/Tests/AbstractTest.java
+++ src/test/java/Tests/AbstractTest.java
@@ -21,7 +21,7 @@ public abstract class AbstractTest {
     @Value("${dbPassword}")
     private String dbPassword;
 
-    @Value("${userName}")
+    @Value("${dbUserName}")
     private String userName;
 
     @Value("${jdbcdriver}")
@@ -32,23 +32,35 @@ public abstract class AbstractTest {
 
 
     @Before
-    public void setUp(){
-        truncateDatabase();
+    public void setUp() {
+        Connection conn = initConnection();
+        truncateDatabase(conn);
     }
 
-    public void truncateDatabase(){
-//        executeSQL("CALL netapp.TruncateTables");
-//        executeSQL("DROP DATABASE netapp");
-    }
-
-
-    private void executeSQL(String sql) {
-        Connection conn = null;
-        Statement stmt = null;
-
+    private Connection initConnection() {
         try {
-            Class.forName(jdbcdriver);
-            conn = DriverManager.getConnection(connectionURL, userName, dbPassword);
+            return DriverManager.getConnection(connectionURL, userName, dbPassword);
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    private void truncateDatabase(Connection conn) {
+        try {
+            String catalog = conn.getCatalog();
+            executeSQL(conn, "CALL " + catalog + ".TruncateTables");
+            //        executeSQL("DROP DATABASE netapp");
+        } catch (Exception e) {
+
+        }
+    }
+
+
+    private void executeSQL(Connection conn, String sql) {
+        Statement stmt = null;
+        try {
+//            Class.forName(jdbcdriver);
             stmt = conn.createStatement();
             stmt.execute(sql);
         } catch (Exception e) {
