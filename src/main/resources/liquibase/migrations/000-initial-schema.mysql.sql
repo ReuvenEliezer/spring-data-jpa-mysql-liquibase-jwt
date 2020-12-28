0d48c17f2aad5f5c5130b76eff2d74b2524809fe
Eliezer <reuvene@mprest.com>
2020-12-28 23:49:45 +0200

    update maven version

--- src/main/resources/liquibase/migrations/000-initial-schema.mysql.sql
+++ src/main/resources/liquibase/migrations/000-initial-schema.mysql.sql
@@ -15,19 +15,19 @@ FLUSH PRIVILEGES;
 CREATE TABLE  `netapp`.`abstract_entity`(`id`        BIGINT(20)     NOT NULL AUTO_INCREMENT,PRIMARY KEY (`id`));
 
 --changeset reuvene:1511272183089-4
-CREATE TABLE IF NOT EXISTS `netapp`.`note`( `id`        BIGINT(20)     NOT NULL AUTO_INCREMENT,    `Title`     VARCHAR(45) NULL,  `Content`   VARCHAR(45) NULL,`CreatedAt` DATETIME    NOT NULL,`UpdatedAt` DATETIME    NOT NULL,PRIMARY KEY (`id`));
+CREATE TABLE  `netapp`.`note`( `id`        BIGINT(20)     NOT NULL AUTO_INCREMENT,    `Title`     VARCHAR(45) NULL,  `Content`   VARCHAR(45) NULL,`CreatedAt` DATETIME    NOT NULL,`UpdatedAt` DATETIME    NOT NULL,PRIMARY KEY (`id`));
 --     ENGINE = InnoDB
 -- DEFAULT CHARACTER SET = utf8
 --     COLLATE = utf8_bin;
 
 --changeset reuvene:1511272183089-5
-CREATE TABLE IF NOT EXISTS `netapp`.`author`(`id`        BIGINT(20)     NOT NULL AUTO_INCREMENT,
+CREATE TABLE  `netapp`.`author`(`id`        BIGINT(20)     NOT NULL AUTO_INCREMENT,
     `name`     VARCHAR(45)  NULL UNIQUE,
     PRIMARY KEY (`id`)
 );
 
 --changeset reuvene:1511272183089-6
-CREATE TABLE IF NOT EXISTS `netapp`.`book`
+CREATE TABLE  `netapp`.`book`
 (
     `id`        BIGINT(20)     NOT NULL AUTO_INCREMENT,
     `name`     VARCHAR(45) NULL UNIQUE,
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `netapp`.`book`
 );
 
 --changeset reuvene:1511272183089-7
-CREATE TABLE IF NOT EXISTS `employee` (
+CREATE TABLE  `employee` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
@@ -48,14 +48,14 @@ CREATE TABLE IF NOT EXISTS `employee` (
 )
 
 --changeset reuvene:1511272183089-8
-CREATE TABLE IF NOT EXISTS `project` (
+CREATE TABLE  `project` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 )
 
 --changeset reuvene:1511272183089-9
-CREATE TABLE IF NOT EXISTS `employee_project` (
+CREATE TABLE  `employee_project` (
   `employee_id` BIGINT(20) NOT NULL,
   `project_id` BIGINT(20) NOT NULL,
   PRIMARY KEY (`employee_id`,`project_id`),
@@ -67,21 +67,21 @@ CREATE TABLE IF NOT EXISTS `employee_project` (
 )
 
 --changeset reuvene:1511272183089-10
-CREATE TABLE IF NOT EXISTS `course` (
+CREATE TABLE  `course` (
   `id` BIGINT(20)  NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`)
 )
 
 --changeset reuvene:1511272183089-11
-CREATE TABLE IF NOT EXISTS `student` (
+CREATE TABLE  `student` (
   `id` BIGINT(20)  NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`)
 )
 
 --changeset reuvene:1511272183089-12
-CREATE TABLE IF NOT EXISTS `course_rating` (
+CREATE TABLE  `course_rating` (
   `course_id` BIGINT(20)  NOT NULL,
   `student_id` BIGINT(20)  NOT NULL,
   `rating` DOUBLE NULL,
