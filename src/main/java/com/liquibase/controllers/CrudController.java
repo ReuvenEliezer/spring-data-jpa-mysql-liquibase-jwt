6d258805d3b5d54fd1e0cd162bb9759bf8626ebf
Eliezer <reuvene@mprest.com>
2021-11-22 19:43:27 +0200

    change @DeleteMapping

--- src/main/java/com/liquibase/controllers/CrudController.java
+++ src/main/java/com/liquibase/controllers/CrudController.java
@@ -3,10 +3,7 @@ package com.liquibase.controllers;
 import com.liquibase.client_entities.AbstractEntityViewModel;
 import com.liquibase.services.web.EntityWebService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,22 +13,22 @@ public abstract class CrudController<VM extends AbstractEntityViewModel, ID> {
     protected EntityWebService<VM, ID> webService;
 
 
-    @PostMapping(value = "/saveOrUpdate")
+    @PostMapping()
     public VM saveOrUpdate(@RequestBody VM entity) {
         return webService.saveOrUpdate(entity);
     }
 
-    @GetMapping(value = "/findById/{id}")
+    @GetMapping(value = "{id}")
     public VM findById(@PathVariable ID id) {
         return webService.findById(id);
     }
 
-    @GetMapping(value = "/findAll")
+    @GetMapping(value = "findAll")
     public List<VM> findAll() {
         return webService.findAll();
     }
 
-    @GetMapping(value = "/delete/{id}")
+    @DeleteMapping(value = "{id}")
     public void delete(@PathVariable ID id) {
         webService.delete(id);
     }
