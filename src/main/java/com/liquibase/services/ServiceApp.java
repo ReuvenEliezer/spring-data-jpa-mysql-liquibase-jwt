c876e895036d3e843dcbd93de92b12845e74cf52
eliezer <eliezer884@gmail.com>
2021-12-14 22:23:41 +0200

    add logger

--- src/main/java/com/liquibase/services/ServiceApp.java
+++ src/main/java/com/liquibase/services/ServiceApp.java
@@ -1,6 +1,12 @@
 package com.liquibase.services;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface ServiceApp {
+
+    Logger logger = LoggerFactory.getLogger(ServiceApp.class);
 
     void start();
 
