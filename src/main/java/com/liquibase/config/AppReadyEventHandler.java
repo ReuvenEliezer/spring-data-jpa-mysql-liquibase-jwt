5e5e1d3500bbeec5d9427f29b818bcafc42eb63c
eliezer.reuven@nice.com <144931323+eliezer-nice@users.noreply.github.com>
2025-01-07 11:29:22 +0200

    Fix conflict security bean on test env (#34)
    * fix case-profile
    
    * fix case-profile
    
    * add documentation
    
    * fix mysql-connector-j - take from spring-boot auto-configuration
    
    * fix-conflict-security-bean-on-test-env

--- src/main/java/com/liquibase/config/AppReadyEventHandler.java
+++ src/main/java/com/liquibase/config/AppReadyEventHandler.java
@@ -1,6 +1,9 @@
 package com.liquibase.config;
 
 import com.liquibase.services.ServiceApp;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -10,15 +13,20 @@ import java.util.Map;
 @Component
 public class AppReadyEventHandler implements ApplicationListener<ApplicationReadyEvent> {
 
+    private static final Logger logger = LogManager.getLogger(AppReadyEventHandler.class);
 
     private final Map<String, ServiceApp> services;
+    private final String env;
 
-    public AppReadyEventHandler(Map<String, ServiceApp> services) {
+    public AppReadyEventHandler(Map<String, ServiceApp> services,
+                                @Value("${spring.profiles.active}") String env) {
         this.services = services;
+        this.env = env;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        logger.info("Starting service on {} environment", env);
         for (ServiceApp serviceApp : services.values()) {
             serviceApp.start();
         }
